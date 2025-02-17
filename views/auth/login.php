a0819f8fcc2bb06ef7f6c3943cce02210fbc6c10
Mohammad <Mohammad.hgh.153@gmail.com>
2025-02-17 13:13:50 +0330

    Feat: login form

---
+++ views/auth/login.php
@@ -0,0 +1,6 @@
+<form method="POST">
+    ایمیل: <input type="email" name="email" required><br>
+    رمز عبور: <input type="password" name="password" required><br>
+    <button type="submit">ورود</button>
+</form>
+<a href="/register">ثبت‌نام</a>
