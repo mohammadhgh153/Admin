72423d3cb98817f3e7e507e715a24c1533a8e1d8
Mohammad <Mohammad.hgh.153@gmail.com>
2025-02-17 13:09:24 +0330

    Feat: show user list

---
+++ app/controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__ . "/../models/User.php";
+session_start();
+
+class UserController {
+    public function dashboard() {
+        if (!isset($_SESSION["user_id"])) {
+            header("Location: /login");
+            exit;
+        }
+
+        $userModel = new User();
+        $users = $userModel->getAllUsers();
+
+        require_once __DIR__ . "/../../views/user/dashboard.php";
+    }
+}
+?>
