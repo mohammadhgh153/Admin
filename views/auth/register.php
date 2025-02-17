cb6d93506d7c3c736916c7bef38cdcd00f5f5598
Mohammad <Mohammad.hgh.153@gmail.com>
2025-02-17 13:12:29 +0330

    Register form

---
+++ views/auth/register.php
@@ -0,0 +1,7 @@
+<form method="POST">
+    نام: <input type="text" name="name" required><br>
+    ایمیل: <input type="email" name="email" required><br>
+    رمز عبور: <input type="password" name="password" required><br>
+    <button type="submit">ثبت‌نام</button>
+</form>
+<a href="/login">ورود</a>
